01b40413a97ff2a6844c1f569ba8ede3188d1d24
MonojitD <monojitdeb66@gmail.com>
2024-05-19 11:45:40 +0530

    Typo fix

--- src/components/errorModal/ErrorModal.jsx
+++ src/components/errorModal/ErrorModal.jsx
@@ -17,7 +17,7 @@ const ErrorModal = ({ errorMessage }) => {
         <img src={errorIcon} alt="" />
         <p className='error_titile'>{errorMessage}</p>
         <div className="error_msg">
-        It seems there is a network issue with our data service provider when connected through your internet service provider (like Jio networks don't support our API provider). Please switch to a different network to access the content or connecting via a VPN (<Link to='https://windscribe.com/'> you can try Windscribe VPN </Link>).
+        It seems our data service is facing a network issue when connected through your internet service provider ( like Jio networks don't support our API provider ). Please switch to a different network to access the content or just connect again via a VPN (<Link to='https://windscribe.com/'> you can try this VPN </Link>).
         </div>
 
         <button className='button' onClick={refreshPage} >Load again</button>
