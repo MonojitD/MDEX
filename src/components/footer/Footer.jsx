f4c413b8085d7feae441912d9d4d4e57b15977ad
MonojitD <monojitdeb66@gmail.com>
2024-05-19 12:28:35 +0530

    X icon fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -47,7 +47,17 @@ const Footer = () => {
             target="_blank"
             className="icon"
           >
-            <FaTwitter />
+            {/* <FaTwitter /> */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 16 16" fill="none" role="img" aria-labelledby="aoigmmxtgl4m3uxg16v5ojfgjsu6kq1f" class="octicon"><title id="aoigmmxtgl4m3uxg16v5ojfgjsu6kq1f">X</title>
+              <g clip-path="url(#clip0_1668_3024)">
+              <path d="M9.52217 6.77143L15.4785 0H14.0671L8.89516 5.87954L4.76437 0H0L6.24656 8.8909L0 15.9918H1.41155L6.87321 9.78279L11.2356 15.9918H16L9.52183 6.77143H9.52217ZM7.58887 8.96923L6.95596 8.0839L1.92015 1.03921H4.0882L8.15216 6.7245L8.78507 7.60983L14.0677 14.9998H11.8997L7.58887 8.96957V8.96923Z" fill="currentColor"></path>
+              </g>
+              <defs>
+              <clipPath id="clip0_1668_3024">
+              <rect width="16" height="16" fill="white"></rect>
+              </clipPath>
+              </defs>
+            </svg>
           </Link>
           <Link
             to="https://www.linkedin.com/in/monojitdeb"
